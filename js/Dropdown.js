99c02bf3d0fa0eb46a9b495190ee6ae291c74ab0
Loris Marino <loris.mro@gmail.com>
2021-04-04 15:22:37 +0200

    refactor(Dropdown): remove link type

--- js/Dropdown.js
+++ js/Dropdown.js
@@ -1,6 +1,6 @@
 /**********
  * Dropdown
- * v1.0.0
+ * v1.1.0
  */
 
 class Dropdown {
@@ -18,9 +18,6 @@ class Dropdown {
   _initParams() {
     this.isOnMobile = this.$.dropdown.dataset.mobile // Set custom mobile is activated.
     this.name = this.$.dropdown.dataset.name
-    this.type = this.$.dropdown.dataset.type
-      ? this.$.dropdown.dataset.type
-      : 'link' // Set type of dropdown.
     this.items =
       this.type === 'link' ? JSON.parse(this.$.dropdown.dataset.items) : null // Create JSON object with items.
   }
@@ -29,102 +26,46 @@ class Dropdown {
    * Initialize component.
    */
   _initialize() {
-    if (
-      !this.isOnMobile &&
-      this.type === 'link' &&
-      window.matchMedia('(max-width: 991px)').matches
-    ) {
-      // Create the select element.
-      this.$.select = document.createElement('select')
+    this.$.items = Array.from(
+      this.$.dropdown.querySelectorAll('.dropdown__item')
+    )
 
-      // Create the default select option.
-      const $option = document.createElement('option')
-      $option.setAttribute('value', '')
-      $option.innerText = this.$.dropdown.dataset.label
-      this.$.select.appendChild($option)
+    // Create the content with elements.
+    const $content = document.createElement('div')
+    $content.classList.add('dropdown__content')
 
-      // Create select options.
-      for (const item of this.items) {
-        const $option = document.createElement('option')
-        $option.setAttribute('value', item.link)
-        $option.innerText = item.label
-        this.$.select.appendChild($option)
-      }
-
-      this.$.dropdown.innerHTML = ''
-      this.$.dropdown.appendChild(this.$.select)
-    } else {
-      if (this.type === 'content')
-        this.$.items = Array.from(
-          this.$.dropdown.querySelectorAll('.dropdown__item')
-        )
-
-      // Create the content with elements.
-      const $content = document.createElement('div')
-      $content.classList.add('dropdown__content')
-
-      if (this.type === 'link') {
-        for (const item of this.items) {
-          const $item = document.createElement('a')
-          $item.innerText = item.label
-          $item.setAttribute('href', item.link)
-
-          $item.classList.add('dropdown__item')
-          $content.appendChild($item)
-        }
-      } else if (this.type === 'content') {
-        for (const $item of this.$.items) {
-          $content.appendChild($item)
-        }
-      }
-
-      // Create the button.
-      this.$.choose = document.createElement('div')
-      this.$.choose.classList.add('dropdown__choose')
-      this.$.choose.innerHTML = this.$.dropdown.dataset.label
-      this.$.choose.setAttribute('id', this.name)
-
-      // Create wrapping container for content.
-      const $container = document.createElement('div')
-      $container.classList.add('dropdown__container')
-      $container.appendChild($content)
-      $container.setAttribute('aria-labelledby', this.name)
-      this.$.dropdown.innerHTML = ''
-      this.$.dropdown.appendChild(this.$.choose)
-      this.$.dropdown.appendChild($container)
-
-      this.$.items = Array.from(
-        this.$.dropdown.querySelectorAll('.dropdown__item')
-      ) // Get all items in array.
+    for (const $item of this.$.items) {
+      $content.appendChild($item)
     }
+
+    // Create the button.
+    this.$.choose = document.createElement('div')
+    this.$.choose.classList.add('dropdown__choose')
+    this.$.choose.innerHTML = this.$.dropdown.dataset.label
+    this.$.choose.setAttribute('id', this.name)
+
+    // Create wrapping container for content.
+    const $container = document.createElement('div')
+    $container.classList.add('dropdown__container')
+    $container.appendChild($content)
+    $container.setAttribute('aria-labelledby', this.name)
+    this.$.dropdown.innerHTML = ''
+    this.$.dropdown.appendChild(this.$.choose)
+    this.$.dropdown.appendChild($container)
 
     this._events()
   }
 
   _events() {
-    if (
-      (!this.isOnMobile && window.matchMedia('(min-width: 992px)').matches) ||
-      this.isOnMobile ||
-      (!this.isOnMobile && this.type === 'content')
-    ) {
-      this.$.choose.addEventListener('click', () => {
-        this.$.dropdown.classList.toggle('dropdown--open')
-      })
+    this.$.choose.addEventListener('click', () => {
+      this.$.dropdown.classList.toggle('dropdown--open')
+    })
 
-      document.addEventListener('click', event => {
-        if (!this.$.dropdown.contains(event.target)) {
-          this.$.dropdown.classList.remove('dropdown--open')
-        }
-      })
-    } else if (
-      !this.isOnMobile &&
-      this.type === 'link' &&
-      window.matchMedia('(max-width: 991px)').matches
-    ) {
-      this.$.select.addEventListener('change', () => {
-        window.location = this.$.select.value
-      })
-    }
+    document.addEventListener('click', event => {
+      if (!this.$.dropdown.contains(event.target)) {
+        this.$.dropdown.classList.remove('dropdown--open')
+      }
+    })
 
     window.addEventListener('resize', () => {
       this._initialize()
